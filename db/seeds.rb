969139a23d50ce32914083842ec63ad87eae6f82
Arthur Lurye <arthurlurye@gmail.com>
2018-06-19 13:16:03 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+turing = Room.create(name: "Turing")
+kay = Room.create(name:"Kay")
+babbage = Room.create(name:"Babbage")
+phone_booth_1 = Room.create(name:"Phone Booth 1")
+phone_booth_2 = Room.create(name:"Phone Booth 2")
+phone_booth_3 = Room.create(name:"Phone Booth 3")
+phone_booth_4 = Room.create(name:"Phone Booth 4")
+phone_booth_5 = Room.create(name:"Phone Booth 5")
+phone_booth_6 = Room.create(name:"Phone Booth 6")
