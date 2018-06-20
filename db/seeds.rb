b079432357a041e44cdfff91837b7ebd05fc50d2
Daniel Brazel <dbrazel4@gmail.com>
2018-06-20 17:59:45 -0400

    Added some evnts to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,14 @@ phone_booth_3 = Room.create(name:"Phone Booth 3")
 phone_booth_4 = Room.create(name:"Phone Booth 4")
 phone_booth_5 = Room.create(name:"Phone Booth 5")
 phone_booth_6 = Room.create(name:"Phone Booth 6")
+
+
+Event.create(title:"End of Year Party", time:"2018-06-19T19:11:11.548Z", room_name:"Turing", room_id:1)
+Event.create(title:"Boogie", time:"2018-06-20T19:11:11.548Z", room_name:"Turing", room_id:1)
+Event.create(title:"Tri-Wizard Tournament", time:"2018-06-21T19:11:11.548Z", room_name:"Turing", room_id:1)
+Event.create(title:"Schmooze with Booze", time:"2018-06-30T19:11:11.548Z", room_name:"Turing", room_id:1)
+
+Event.create(title:"Oktober Fest", time:"2018-10-25T19:11:11.548Z", room_name:"Kay", room_id:2)
+Event.create(title:"Test Grading", time:"2018-06-25T19:11:11.548Z", room_name:"Kay", room_id:2)
+Event.create(title:"Community Lunch", time:"2018-06-24T12:11:11.548Z", room_name:"Kay", room_id:2)
+Event.create(title:"Oktober Fest", time:"2018-10-25T19:11:11.548Z", room_name:"Kay", room_id:2)
